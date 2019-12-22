683eb7ec7f6af557a5c2b19f7770eea58358c829
Kirill Savinkov <kirisav@yandex.ru>
2019-12-22 23:53:47 +0300

    add lab4

---
+++ lab_4/lab4-zuul-server/src/main/java/com/example/lab4zuulserver/Lab4ZuulServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab4zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4ZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4ZuulServerApplication.class, args);
+	}
+
+}
